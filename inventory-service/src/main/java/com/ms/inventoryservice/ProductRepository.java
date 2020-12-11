259719bd92c31e2b9fcfecc75c40f5ebe5884ce4
KaoutarOULAHYANE <46823130+KaoutarOULAHYANE@users.noreply.github.com>
2020-12-11 13:55:02 +0100

    commit INVENTORY-SERVICE

---
+++ inventory-service/src/main/java/com/ms/inventoryservice/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ms.inventoryservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation
+        .RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
