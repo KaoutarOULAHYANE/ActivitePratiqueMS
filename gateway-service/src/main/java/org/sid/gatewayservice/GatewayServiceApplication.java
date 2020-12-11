7beff59a8c4c58933a536c8b8d54dc6c1ef012b8
KaoutarOULAHYANE <46823130+KaoutarOULAHYANE@users.noreply.github.com>
2020-12-11 13:58:49 +0100

    commit GATEWAY-SERVICE

---
+++ gateway-service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,44 @@
+package org.sid.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    /*Configuration statique*/
+    /*Configuration JAVA de Spring Cloud Gateway*/
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/customers/**").uri("http://localhost:8081/"))
+                .route(r -> r.path("/products/**").uri("http://localhost:8082/"))
+                .build();
+    }
+
+    @Bean
+    RouteLocator routeLocator2(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .build();
+    }
+
+    /*Configuration dynamique*/
+    /*http://localhost:8888/CUSTOMER-SERVICE/customers*/
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient discoveryClient,
+                                                            DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient,properties);
+    }
+}
