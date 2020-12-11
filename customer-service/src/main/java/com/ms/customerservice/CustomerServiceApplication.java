e385a63f8549f2011ab424a80feb3f530ff5dc0c
KaoutarOULAHYANE <46823130+KaoutarOULAHYANE@users.noreply.github.com>
2020-12-11 13:51:31 +0100

    commit CUSTOMER-SERVICE

---
+++ customer-service/src/main/java/com/ms/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,27 @@
+package com.ms.customerservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner start(
+            CustomerRepository customerRepository,
+            RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Customer.class);
+        return args -> {
+            customerRepository.save(new Customer(null,"OULAHYANE Kaoutar","kaoutar.oulahyane@gmail.com"));
+            customerRepository.save(new Customer(null,"OULAHYANE Zaina","zaina.oulahyane@gmail.com"));
+            customerRepository.findAll().forEach(customer -> System.out.println(customer));
+        };
+    }
+}
